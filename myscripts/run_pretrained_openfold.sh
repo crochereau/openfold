04a2e1564855989bbd25b245d1a9c419fee587ba
crochereau <charlotte.rochereau@gmail.com>
2023-04-20 17:00:37 -0400

    Update config and script to run OF.

--- myscripts/run_pretrained_openfold.sh
+++ myscripts/run_pretrained_openfold.sh
@@ -8,5 +8,5 @@ python3 run_pretrained_openfold.py \
 --use_precomputed_alignments ../function_pred/data/alignments/ \
 --output_dir ../function_pred/data/alphafold/test/ \
 --model_device "cuda:0" \
---jax_param_path data/params/params_model_1.npz \
+--jax_param_path ../../../projects/resources/alphafold_params/alphafold_params/params_model_1.npz \
 --save_single
