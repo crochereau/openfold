60681ad1ef6cf885192a23eaf5162a908e3eb141
crochereau <charlotte.rochereau@gmail.com>
2023-04-20 15:45:40 -0400

    New directory with own scripts. Add bash script for running pretrained AlphaFold in server.

---
+++ myscripts/run_pretrained_openfold.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+source scripts/activate_conda_env.sh
+
+python3 run_pretrained_openfold.py \
+../function_pred/data/fastas/ \
+../function_pred/data/pdb_mmcif/mmcif_files/ \
+--use_precomputed_alignments ../function_pred/data/alignments/ \
+--output_dir ../function_pred/data/alphafold/test/ \
+--model_device "cuda:0" \
+--jax_param_path data/params/params_model_1.npz \
+--save_single
